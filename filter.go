3afbf90d7bd85d0c54db628b792c8760ed17ed52
voje <>
2021-05-24 19:01:45 +0200

    Intermediate commit - checking dns request contents

--- filter.go
+++ filter.go
@@ -5,17 +5,25 @@ import (
 
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
 	"github.com/miekg/dns"
 )
 
 var log = clog.NewWithPlugin("filter")
 
 type Filter struct {
-	Next plugin.Handler
+	Next      plugin.Handler
+	Blacklist map[string]bool
 }
 
 func (h Filter) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	log.Info("Testing filter")
+
+	state := request.Request{W: w, Req: r}
+	log.Infof("%+v", state)
+
+	// TODO
+
 	return h.Next.ServeDNS(ctx, w, r)
 }
 
--- setup.go
+++ setup.go
@@ -1,6 +1,10 @@
 package filter
 
 import (
+	"bufio"
+	"os"
+	"strings"
+
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
@@ -9,8 +13,43 @@ import (
 func init() { plugin.Register("filter", setup) }
 
 func setup(c *caddy.Controller) error {
+	var filter Filter
+	filter.Blacklist = make(map[string]bool)
+
+	// Read blacklist
+	c.Next() // Skip 'filter'
+	hasNextToken := c.NextArg()
+	if !hasNextToken {
+		return plugin.Error("filter plugin's argument is the path to a blacklist file", c.ArgErr())
+	}
+	fileName := c.Val()
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return plugin.Error("Failed reading file: "+fileName, err)
+	}
+	defer file.Close()
+
+	// Save blacklist entries into 'filter'
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		line := s.Text()
+		line = strings.Trim(line, " \n\t\r")
+		if line == "" {
+			continue
+		}
+		filter.Blacklist[line] = true
+	}
+
+	// Check for read errors
+	err = s.Err()
+	if err != nil {
+		return plugin.Error("Error while reading file", err)
+	}
+
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-		return Filter{Next: next}
+		filter.Next = next
+		return filter
 	})
 	return nil
 }
