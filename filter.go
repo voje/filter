20b9c1fd6c7d6c14aef7c9028cff9b5778dc13d4
voje <>
2021-05-24 19:43:46 +0200

    Adds redirection logic form kubecon workshop

--- filter.go
+++ filter.go
@@ -2,6 +2,7 @@ package filter
 
 import (
 	"context"
+	"net"
 
 	"github.com/coredns/coredns/plugin"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
@@ -21,11 +22,25 @@ func (h Filter) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) 
 	fqdn := r.Question[0].Name
 	log.Info("fqdn: " + fqdn)
 
-	redirectFqdn := "localhost"
-
 	if _, ok := h.Blacklist[fqdn]; ok {
-		log.Infof("Redirecting: %s => %s", redirectFqdn)
-		r.Question[0].Name = redirectFqdn // redirect to localhost
+		reply := "127.0.0.1"
+		log.Infof("Redirecting: %s => %s", fqdn, reply)
+
+		answers := []dns.RR{}
+
+		rr := new(dns.A)
+		rr.Hdr = dns.RR_Header{Name: "localhost", Rrtype: dns.TypeA, Class: dns.ClassINET}
+		rr.A = net.ParseIP(reply).To4()
+
+		answers = append(answers, rr)
+
+		m := new(dns.Msg)
+		m.SetReply(r)
+		m.Authoritative = true
+		m.Answer = answers
+
+		w.WriteMsg(m)
+		return dns.RcodeSuccess, nil
 	}
 
 	return h.Next.ServeDNS(ctx, w, r)
