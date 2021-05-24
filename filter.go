122d4334e99328667aa7a65e4ca1c00be8564c8d
voje <>
2021-05-24 16:03:55 +0200

    Adds log

--- filter.go
+++ filter.go
@@ -4,14 +4,18 @@ import (
 	"context"
 
 	"github.com/coredns/coredns/plugin"
+	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/miekg/dns"
 )
+
+var log = clog.NewWithPlugin("filter")
 
 type FilterHandler struct {
 	Next plugin.Handler
 }
 
 func (h FilterHandler) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	log.Debug("Testing filter")
 	return h.Next.ServeDNS(ctx, w, r)
 }
 
